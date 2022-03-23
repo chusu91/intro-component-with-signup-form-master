12b4f2db364afc0a97b6633c12dcda84fe95f6cf
chusu91 <79313766+chusu91@users.noreply.github.com>
2022-03-23 11:30:58 +0200

    JS add error Icon

--- main.js
+++ main.js
@@ -6,38 +6,32 @@ const inputLastName = document.querySelector(".form_last-name");
 const inputEmail = document.querySelector(".form_email");
 const inputPassword = document.querySelector(".form_password");
 
-let errorMsgFirstName = 0;
-let errorMsgLastName;
-let errorMsgEmail;
-let errorMsgPassword;
-
 function onHandleSubmit(e) {
   e.preventDefault();
-  checkInput(inputFirstName, errorMsgFirstName);
-  checkInput(inputLastName, errorMsgLastName);
-  checkInput(inputPassword, errorMsgEmail);
-  checkEmail(inputEmail, errorMsgPassword);
+  checkInput(inputFirstName);
+  checkInput(inputLastName);
+  checkInput(inputPassword);
+  checkEmail(inputEmail);
 }
 
-function checkInput(input, errorMsg) {
+function checkInput(input) {
   const inputContainer = input.parentNode;
   const inputValue = input.value;
   const inputCategory = input.placeholder;
+  let error = false;
 
-  if (inputValue == "") {
+  if (inputValue == "" && !error) {
     //add &&condtion that span will be added only once..
-    //ooops.... outline css dissappered..
-    // how to add img to input?? so that placeholder text will be dissapeard?
-    //const errorIcon = document.createElement("img");
-    //errorIcon.src = "./images/icon-error.svg"
-    //inputValue = errorIcon;
+
     const span = document.createElement("span");
     span.setAttribute("class", "is-error");
     span.innerText = `${inputCategory} cannot be empty`;
     inputContainer.appendChild(span); // maybe api that delete the previous sibling node before append???
     input.style.outline = "2px solid var(--color-Red)";
-    errorMsg === 1;
-    console.log(input, errorMsg); // think how to use variable to all the inputs...
+    console.log(input);
+    input.classList.add("error-icon");
+    error = true;
+    // think how to use variable to all the inputs...
     //global scope let variable.. changed to true at the end of fuction.. but did not changed in the global scope?
   }
 }
